6e4e0beb839e660519d84751590bf82e3e94a155
JLPinheiro1971m <joseluispinheiro1971@gmail.com>
2023-12-11 16:51:50 +0000

    Versão 0.3 com melhorias gerais

--- scripts/script.js
+++ scripts/script.js
@@ -3,41 +3,69 @@ function exibirAlertaAposTempo(tempo) {
     //alert("Bem-vindo ao nosso site!");
   }, tempo);
 }
+
 exibirAlertaAposTempo(5000);
 
-function exibirAlertaSimples() {
+// FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE FORMULÁRIO DE CONTATO
+function confirmarPreenchimentoContato() {
+  var contatoNome = document.getElementById('contatoNome').value;
+  var contatoApelido = document.getElementById('contatoApelido').value;
+  var contatoTelemovel = document.getElementById('contatoTelemovel').value;
+  var contatoEmail = document.getElementById('contatoEmail').value;
+  var contatoData = document.getElementById('contatoData').value;
+  var contatoMotivo = document.getElementById('contatoMotivo').value;
+  if (contatoNome == '' || contatoApelido == '' || contatoTelemovel == '' || contatoEmail == '' || contatoData == '' || contatoMotivo == '') {
+    alert('Todos os campos do formulário de contato precisam estar preenchidos.');
+  }
+}
+
+// FUNÇÃO PARA CALCULAR ORÇAMENTO
+function calcularOrcamento() {
   const extra = 400;
+  var prazo = 1;
   var desc = 1;
   var a = parseFloat(document.getElementById('inputTipoSite').value);
-  var meses = parseFloat(document.getElementById('inputPrazo').value);  
+  var prazo = parseFloat(document.getElementById('inputPrazo').value);
   var c = $('input[type=checkbox]:checked').length;
-  if (meses == 2) {
-    desc = 0.95;  
-  }
 
-  if (meses == 3) {
-    desc = 0.9;  
-  }
+  if (prazo == 2) {
+    desc = 0.95;
+  } else if (prazo == 3) {
+    desc = 0.9;
+  } else if (prazo == 4) {
+    desc = 0.9;
+  } else if (prazo == 5) {
+    desc = 0.85;
+  } else if (prazo > 5) {
+    desc = 0.8;  }
 
-  if (meses == 4) {
-    desc = 0.9;  
-  }
-
-  if (meses == 5) {
-    desc = 0.85;  
-  }
-
-  if (meses > 5) {
-    desc = 0.8;  
-  }
   var total = (a + (extra * c)) * desc;
-  alert("Valor total: " + total + " €");
+
+  document.getElementById('resultado').value = total;
+  var resul = parseFloat(document.getElementById('resultado').value);
+
+}
+
+// FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE FORMULÁRIO DE PEDIDO DE ORÇAMENTO
+function enviarOrcamento() {
+  
+  var orcamentoNome = document.getElementById('orcamentoNome').value;
+  var orcamentoApelido = document.getElementById('orcamentoApelido').value;
+  var orcamentoTelemovel = document.getElementById('orcamentoTelemovel').value;
+  var orcamentoValor = document.getElementById('resultado').value;
+  if (orcamentoNome == '' || orcamentoApelido == '' || orcamentoTelemovel == '') {
+    alert("Os Dados Pessoais precisam estar preenchidos.");
+  } else if (orcamentoValor == 0) {
+    alert('Preencha as informações do pedido de orçamento e itens necessários. Por favor, tente novamente.');
+  } else {
+    alert('O pedido de orçamento foi enviado com sucesso. Em breve retornaremos o contato.');
+  }
 }
 
 
 // EVENTO PARA GERAR UM FEED DE NOTÍCIAS, PEGANDO DADOS DO ARQUIVOS XML EXTERNO
 document.addEventListener("DOMContentLoaded", function () {
-  const url = "scripts/noticias.xml";
+  const url = "scripts/noticias_google.xml";
 
   fetch(url)
     .then(response => response.text())
@@ -65,85 +93,79 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(error => console.error("Erro ao carregar notícias RSS:", error));
 });
 
-// FUNÇÃO GERAR COORDENADAS
-function obterCoordenadas() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
 
-        console.log('Latitude:', latitude);
-        console.log('Longitude:', longitude);
+//INCORPORAÇÃO DO MAPA E CÁLCULO DA ROTA
+document.addEventListener("DOMContentLoaded", function () {
+          getLocation();
+      });
 
-        // Aqui você pode usar as coordenadas conforme necessário
-        // Por exemplo, exibir no mapa usando a API do Google Maps
-        exibirNoMapa(latitude, longitude);
-      },
-      function (error) {
-        console.error('Erro ao obter coordenadas:', error.message);
+      function getLocation() {
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(showPosition, showError);
+          } else {
+              alert("Geolocalização não é suportada pelo seu navegador.");
+          }
       }
-    );
-  } else {
-    console.error('Geolocalização não é suportada neste navegador.');
-  }
-}
 
-function exibirNoMapa(latitude, longitude) {
-  const mapOptions = {
-    center: { lat: latitude, lng: longitude },
-    zoom: 15
-  };
+      function showPosition(position) {
+          var latitude = position.coords.latitude;
+          var longitude = position.coords.longitude;
 
-  const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+          var officeLocation = { lat: 38.73371, lng: -9.14117 }; // Substitua pelas coordenadas reais do seu escritório
 
-  const marker = new google.maps.Marker({
-    position: { lat: latitude, lng: longitude },
-    map: map,
-    title: 'Localização Atual'
-  });
-}
+          // Crie um mapa
+          var map = new google.maps.Map(document.getElementById('map'), {
+              center: { lat: latitude, lng: longitude },
+              zoom: 14
+          });
 
+          // Crie um serviço de direções
+          var directionsService = new google.maps.DirectionsService;
+          var directionsDisplay = new google.maps.DirectionsRenderer({
+              map: map
+          });
 
-// FUNÇÃO INIT MAP
+          // Configure a solicitação de direções
+          var request = {
+              origin: { lat: latitude, lng: longitude },
+              destination: officeLocation,
+              travelMode: 'DRIVING'
+          };
 
-function initMap() {
-  // Substitua as coordenadas pelos valores reais
-  const escritorio = { lat: 41.15635, lng: -8.61010 }; // Coordenadas da MasterD
-  const cliente = { lat: 41.549053, lng: -8.433865 };   // Coordenadas do comboio de Braga
+          // Faça a solicitação de direções
+          directionsService.route(request, function(response, status) {
+              if (status === 'OK') {
+                  // Exiba as direções no mapa
+                  directionsDisplay.setDirections(response);
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: escritorio,
-    zoom: 14
-  });
+                  // Exiba informações sobre a rota
+                  var route = response.routes[0];
+                  displayRouteInfo(route);
+              } else {
+                  window.alert('Erro ao calcular a rota: ' + status);
+              }
+          });
+      }
 
-  const markerEscritorio = new google.maps.Marker({
-    position: escritorio,
-    map: map,
-    title: 'Escritório'
-  });
+      function displayRouteInfo(route) {
+          var infoDiv = document.getElementById('info');
+          infoDiv.innerHTML = 'Tempo de Deslocamento: ' + route.legs[0].duration.text + '<br>' +
+                              'Distância: ' + route.legs[0].distance.text;
+      }
 
-  const markerCliente = new google.maps.Marker({
-    position: cliente,
-    map: map,
-    title: 'Cliente'
-  });
-
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  directionsRenderer.setMap(map);
-
-  const request = {
-    origin: cliente,
-    destination: escritorio,
-    travelMode: 'DRIVING'
-  };
-
-  directionsService.route(request, function (response, status) {
-    if (status === 'OK') {
-      directionsRenderer.setDirections(response);
-    } else {
-      window.alert('Erro ao calcular o caminho: ' + status);
-    }
-  });
-}
+      function showError(error) {
+          switch (error.code) {
+              case error.PERMISSION_DENIED:
+                  alert("Permissão de geolocalização negada pelo usuário.");
+                  break;
+              case error.POSITION_UNAVAILABLE:
+                  alert("Informações de localização indisponíveis.");
+                  break;
+              case error.TIMEOUT:
+                  alert("A solicitação para obter a localização do usuário expirou.");
+                  break;
+              case error.UNKNOWN_ERROR:
+                  alert("Ocorreu um erro desconhecido ao tentar obter a localização do usuário.");
+                  break;
+          }
+      }
